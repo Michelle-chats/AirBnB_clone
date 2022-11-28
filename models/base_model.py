3e02741bab2ac8288b8582d711b00516d93b4207
Michelle-chats <mtchatikobo@gmail.com>
2022-11-28 01:02:56 -0800

    submitting

--- models/base_model.py
+++ models/base_model.py
@@ -40,3 +40,19 @@ class BaseModel:
         of an instance."""
         return "[{}] ({}) {}".\
                 format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the updated_at attribute
+        with the current datetime."""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary representation of an instance."""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
